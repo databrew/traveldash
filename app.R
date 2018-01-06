fe14cdfe75f8717d4a2dac2a766f7c99eea92e71
joebrew <joebrew@gmail.com>
2018-01-06 08:33:47 +0100

    editing menu almost there - still tripping on modify

--- app.R
+++ app.R
@@ -102,8 +102,7 @@ body <- dashboardBody(
     ),
     tabItem(
       tabName = 'edit_data',
-      fluidPage(
-      ))
+      uiOutput("MainBody"))
     
   ))
 # UI
@@ -192,27 +191,42 @@ server <- function(input, output, session) {
   vals$Data<-filter_events(events = events,
                            visit_start = min(date_dictionary$date),
                            visit_end = max(date_dictionary$date))
-  observeEvent(filter_dates(),{
-    fd <- filter_dates()
-    ev <- vals$Data
-    vals$Data <- filter_events(events = ev,
-                               visit_start = fd[1],
-                               visit_end = fd[2])
+  observeEvent(input$Del_row_head, {
+    vals$Data <- vals$Data
   })
-  observeEvent(input$search, {
-    ev <- vals$Data
-    vals$Data <- filter_events(events = ev,
-                               search = input$search)
-  })
+
+  # # observeEvent(filter_dates(),{
+  # #   fd <- filter_dates()
+  # #   print(fd)
+  # #   ev <- vals$Data
+  # #   print(head(ev))
+  # #   # ev
+  # #   vals$Data <- filter_events(events = ev,
+  # #                              visit_start = fd[1],
+  # #                              visit_end = fd[2])
+  # #   print(head(vals$Data))
+  # # })
+  # observeEvent({input$search
+  #   # filter_dates()
+  #   }, {
+  #   # fd <- filter_dates()
+  #   message(fd)
+  #   ev <- vals$Data
+  #   vals$Data <- filter_events(events = ev,
+  #                              # visit_start = fd[1],
+  #                              # visit_end = fd[2],
+  #                              search = input$search)
+  # })
   filtered_events <- reactive({
+    # x <- vals$Data
     fd <- filter_dates()
-    x <- filter_events(events = events,
+    x <- filter_events(events = vals$Data,
                        visit_start = fd[1],
                        visit_end = fd[2],
                        search = input$search)
     # Jitter if necessary
-    if(any(duplicated(events$Lat)) |
-       any(duplicated(events$Long))){
+    if(any(duplicated(x$Lat)) |
+       any(duplicated(x$Long))){
       x <- x %>%
         mutate(Long = jitter(Long, factor = 0.5),
                Lat = jitter(Lat, factor = 0.5))
@@ -380,6 +394,191 @@ server <- function(input, output, session) {
   })
   
   selected_date <- reactive({input$selected_date})
+  
+  output$MainBody<-renderUI({
+    fluidPage(
+      box(width=12,
+          h3(strong("Create, modify, and delete travel events"),align="center"),
+          hr(),
+          column(6,offset = 6,
+                 HTML('<div class="btn-group" role="group" aria-label="Basic example">'),
+                 actionButton(inputId = "Add_row_head",label = "Add a new row"),
+                 actionButton(inputId = "Del_row_head",label = "Delete selected rows"),
+                 HTML('</div>')
+          ),
+          
+          column(12,dataTableOutput("Main_table")),
+          tags$script(HTML('$(document).on("click", "input", function () {
+                           var checkboxes = document.getElementsByName("row_selected");
+                           var checkboxesChecked = [];
+                           for (var i=0; i<checkboxes.length; i++) {
+                           
+                           if (checkboxes[i].checked) {
+                           checkboxesChecked.push(checkboxes[i].value);
+                           }
+                           }
+                           Shiny.onInputChange("checked_rows",checkboxesChecked);
+  })')),
+      tags$script("$(document).on('click', '#Main_table button', function () {
+                  Shiny.onInputChange('lastClickId',this.id);
+                  Shiny.onInputChange('lastClick', Math.random())
+});")
+
+      )
+      )
+    })
+  
+  output$Main_table<-renderDataTable({
+    DT=vals$Data
+    DT[["Select"]]<-paste0('<input type="checkbox" name="row_selected" value="Row',1:nrow(vals$Data),'"><br>')
+    
+    DT[["Actions"]]<-
+      paste0('
+             <div class="btn-group" role="group" aria-label="Basic example">
+             <button type="button" class="btn btn-secondary delete" id=delete_',1:nrow(vals$Data),'>Delete</button>
+             <button type="button" class="btn btn-secondary modify"id=modify_',1:nrow(vals$Data),'>Modify</button>
+             </div>
+             
+             ')
+    datatable(DT,
+              escape=F,
+              options = list(scrollX = TRUE))}
+      )
+  
+  observeEvent(input$Add_row_head,{
+    new_row=data_frame(
+      Person = 'Jane Doe',
+      Organization = 'World Bank',
+      `City of visit` = 'New York',
+      `Country of visit` = 'United States',
+      Counterpart = 'Donald Trump',
+      `Visit start` = Sys.Date() - 3,
+      `Visit end` = Sys.Date())
+    new_row <- new_row %>%
+      mutate(`Visit month` = format(`Visit start`, '%B')) %>%
+      mutate(Lon = -74.00597,
+             Lat = 40.71278)
+    # place <- paste0(new_row$`City of visit`, ', ', new_row$`Country of visit`)
+    # ll <- ggmap::geocode(location = place, output = 'latlon')
+    # new_row$Long <- ll$lon
+    # new_row$Lat <- ll$lat
+    new_row$file <- 'headshots/circles/new.png'
+    vals$Data<-bind_rows(new_row,vals$Data)
+  })
+  
+  
+  observeEvent(input$Del_row_head,{
+    row_to_del=as.numeric(gsub("Row","",input$checked_rows))
+    
+    vals$Data=vals$Data[-row_to_del,]}
+  )
+
+  output$sales_plot<-renderPlot({
+    require(ggplot2)
+    ggplot(vals$fake_sales,aes(x=month,y=sales,color=Brands))+geom_line()
+  })
+  
+  ##Managing in row deletion
+  modal_modify<-modalDialog(
+    fluidPage(
+      h3(strong("Row modification"),align="center"),
+      hr(),
+      dataTableOutput('row_modif'),
+      actionButton("save_changes","Save changes"),
+      
+      tags$script(HTML("$(document).on('click', '#save_changes', function () {
+                       var list_value=[]
+                       for (i = 0; i < $( '.new_input' ).length; i++)
+                       {
+                       list_value.push($( '.new_input' )[i].value)
+                       
+                       
+                       
+                       }
+                       
+                       Shiny.onInputChange('newValue', list_value)
+                       });"))
+    ),
+    size="l"
+      )
+  
+  
+  observeEvent(input$lastClick,
+               {
+                 if (input$lastClickId%like%"delete")
+                 {
+                   row_to_del=as.numeric(gsub("delete_","",input$lastClickId))
+                   vals$Data=vals$Data[-row_to_del,]
+                 }
+                 else if (input$lastClickId%like%"modify")
+                 {
+                   showModal(modal_modify)
+                 }
+               }
+  )
+  
+  output$row_modif<-renderDataTable({
+    selected_row=as.numeric(gsub("modify_","",input$lastClickId))
+    old_row=vals$Data[selected_row,]
+    the_dates <- the_nums <- rep(FALSE, ncol(old_row))
+    for(j in 1:ncol(old_row)){
+      if(class(data.frame(old_row)[,j]) == 'Date'){
+        the_dates[j] <- TRUE
+      } else if (class(data.frame(old_row)[,j]) %in% c('numeric', 'integer')){
+        the_nums[j] <- TRUE
+      }
+    }
+    row_change=list()
+    for (i in colnames(old_row)){
+      # if(class(vals$Data[[i]])[1] == 'Date'){
+      #   row_change[[i]]<-paste0('<input class="new_input" type="date" id=new_',i,'><br>')
+      # } else 
+        if (is.numeric(vals$Data[[i]]))
+      {
+        row_change[[i]]<-paste0('<input class="new_input" type="number" id=new_',i,'><br>')
+      }
+      else
+        row_change[[i]]<-paste0('<input class="new_input" type="text" id=new_',i,'><br>')
+    }
+    row_change=as.data.table(row_change)
+    setnames(row_change,colnames(old_row))
+    for(j in which(the_dates)){
+      old_row[,j] <- as.character(old_row[,j])
+    }
+    for(j in which(the_nums)){
+      old_row[,j] <- as.character(old_row[,j])
+    }
+    DT=bind_rows(old_row,row_change)
+    rownames(DT)<-c("Current values","New values")
+    for(j in which(the_dates)){
+      old_row[,j] <- as.Date(as.numeric(old_row[,j]), origin = '1970-01-01')
+    }
+    for(j in which(the_nums)){
+      old_row[,j] <- as.numeric(old_row[,j])
+    }
+    
+    DT
+    
+  },escape=F,options=list(dom='t',ordering=F),selection="none"
+  )
+  
+  
+  observeEvent(input$newValue,
+               {
+                 newValue=lapply(input$newValue, function(col) {
+                   if (suppressWarnings(all(!is.na(as.numeric(as.character(col)))))) {
+                     as.numeric(as.character(col))
+                   } else {
+                     col
+                   }
+                 })
+                 DF=data_frame(lapply(newValue, function(x) t(data_frame(x))))
+                 colnames(DF)=colnames(vals$Data)
+                 vals$Data[as.numeric(gsub("modify_","",input$lastClickId))]<-DF
+                 
+               }
+  )
+  
   
   }
 
