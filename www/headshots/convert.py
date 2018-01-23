d465a64aeb13975f7b72bfe61abe4b7d58e293d7
joebrew <joebrew@gmail.com>
2018-01-23 17:38:51 +0100

    image conversion improvements

--- www/headshots/convert.py
+++ www/headshots/convert.py
@@ -1,11 +1,12 @@
 import os
 import re
 photos = os.listdir(os.getcwd())
+photos = ['Rex Tillerson.png', 'Mike Pence.png', 'Donald Trump.png']
 for i in range(0, (len(photos)-1)):
 	this_photo = photos[i]#.split('xxxx')
 	this_photo = re.sub(r' ', '\ ', this_photo)
 	print this_photo
 	if '.png' in this_photo:
-		conversion_text = 'convert -size 200x200 -bordercolor black -border 1%x1% xc:none -fill ' + this_photo + ' -draw "circle 100,100 100,1" circles2/' + this_photo 
+		conversion_text = 'convert -size 200x200 -transparent -border 1%x1% xc:none -fill ' + this_photo + ' -draw "circle 100,100 100,1" circles/' + this_photo 
 		print conversion_text
 		os.system(conversion_text)
