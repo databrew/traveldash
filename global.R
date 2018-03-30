7dbfef17fd121c45add41d9b2f4b972d81c7d27b
joebrew <joebrew@gmail.com>
2018-03-30 15:44:25 +0200

    major timevis overhaul

--- global.R
+++ global.R
@@ -120,13 +120,13 @@ skin <- ifelse(use_sqlite, 'red', 'blue')
 
 # Timevis data prep
 # Get all "events" - which are any period in which someone is at a place continuously
-expand_trips <- function(trips, cities, people){
+expand_trips <- function(trips, cities, people, view_all_trips_people_meetings_venues){
   out <- list()
   for(i in 1:nrow(trips)){
     dates <- seq(trips$trip_start_date[i],
                  trips$trip_end_date[i],
                  by = 1)
-    df <- trips[i,] %>% dplyr::select(trip_id, person_id, city_id)
+    df <- trips[i,] %>% dplyr::select(trip_id, person_id, city_id, trip_uid)
     df1 <- df
     while(length(dates) > nrow(df)){
       df <- bind_rows(df, df1)
@@ -135,12 +135,21 @@ expand_trips <- function(trips, cities, people){
     out[[i]] <- df
   }
   df <- bind_rows(out)
+  # Bring in venue
+  df <- left_join(df,
+                  view_all_trips_people_meetings_venues %>%
+                    group_by(trip_uid) %>%
+                    summarise(venue_name = dplyr::first(venue_name)) %>%
+                    ungroup,
+                  by = 'trip_uid')
+  
   # Get rid of dulicates
-  df <- df %>% dplyr::distinct(city_id, date) %>%
+  df <- df %>% dplyr::distinct(city_id, date,
+                               venue_name) %>%
     arrange(city_id, date)
   # Get gap between previous days
   df <- df %>%
-    group_by(city_id) %>%
+    group_by(city_id,venue_name) %>%
     mutate(date_dif = date - dplyr::lag(date, 1)) %>% ungroup
   # Get a "event_id"
   df$event_id <- NA
@@ -152,6 +161,7 @@ expand_trips <- function(trips, cities, people){
              df$event_id[i-1])
   }
   # Group by event id and get start / end
+  old_df <- df
   df <- df %>%
     group_by(city_id, event_id) %>%
     summarise(start = min(date),
@@ -161,21 +171,36 @@ expand_trips <- function(trips, cities, people){
   df <- left_join(df,
                   cities %>% 
                     dplyr::select(city_id, city_name),
-                  by = 'city_id') %>%
-    dplyr::mutate(content = city_name) %>%
+                  by = 'city_id')
+  # Get the event into
+  df <- left_join(df, old_df, by = c('city_id', 'event_id'))
+  # Make a content variale
+  df <- df %>%
+    mutate(content = ifelse(!is.na(venue_name) & venue_name != '',
+                            venue_name,
+                            city_name)) %>%
+    mutate(content = ifelse(content == 'Unspecified Venue',
+                            paste0('Unspecified event in ', city_name),
+                            content))
+  # Keep only one observation for each event
+  df <- df %>%
+    dplyr::distinct(city_id, event_id, start, end, city_name, content, .keep_all = TRUE)
+  # Remove those with nothing
+  df <- df %>%
     mutate(type = 'range',
-           title = city_name) %>%
+           title = content) %>%
     mutate(group = 1)
   # Get the meetings in each event
-  meetings <- trips %>%
+  meetings <- view_all_trips_people_meetings_venues %>%
     dplyr::select(person_id, city_id, trip_group,
                   trip_start_date,
-                  trip_end_date) %>%
-    left_join(people %>% dplyr::select(person_id, 
-                                       short_name),
-              by = 'person_id') %>%
+                  trip_end_date,
+                  short_name,
+                  agenda) %>%
     dplyr::mutate(content = paste0(short_name, 
-                                   ': ', trip_group)) %>%
+                                   ifelse(!is.na(agenda) & agenda != '',
+                                          ': ',
+                                          ''), agenda)) %>%
     left_join(df %>% dplyr::select(-content,
                                    -type)) %>%
     # Keep only those dates which fall in the range
@@ -233,7 +258,8 @@ if(nrow(view_trips_and_meetings) == 0){
 if(nrow(trips) > 0){
   expanded_trips = expand_trips(trips = trips,
                                 cities = cities,
-                                people = people)  
+                                people = people,
+                                view_all_trips_people_meetings_venues = view_all_trips_people_meetings_venues)  
 } else {
   expanded_trips <- data_frame(city_id = 1,
                                event_id = 1,
