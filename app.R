ec4ef81ee81d0285b90d842ada1ad0a0df4f5a48
joebrew <joebrew@gmail.com>
2018-04-09 09:06:51 +0200

    search url instructions

--- app.R
+++ app.R
@@ -2559,12 +2559,24 @@ server <- function(input, output, session) {
   
   # Return the components of the URL in a string:
   output$url_text <- renderText({
+    the_search <- session$clientData$url_search
+    search_ok <- FALSE
+    if(!is.null(the_search)){
+      if(length(the_search) > 0){
+        if(the_search != ''){
+          search_ok <- TRUE
+        }
+      }
+    }
+    if(!search_ok){
+      the_search <- paste0('Add something like /?foo=123&bar=somestring to the end of the url (and then load the modified url) to see how it is captured here.')
+    }
     paste(sep = "",
           "protocol: ", session$clientData$url_protocol, "\n",
           "hostname: ", session$clientData$url_hostname, "\n",
           "pathname: ", session$clientData$url_pathname, "\n",
           "port: ",     session$clientData$url_port,     "\n",
-          "search: ",   session$clientData$url_search,   "\n"
+          "search: ",   the_search,   "\n"
     )
   })
   
